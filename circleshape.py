89a5f5e312e9bee71c5df13a1b76947f7f90c816
Spillvind <oyvinds.tommernes@gmail.com>
2025-05-20 19:06:42 +0200

    Update

--- circleshape.py
+++ circleshape.py
@@ -15,7 +15,7 @@ class CircleShape(pygame.sprite.Sprite):
 
     def draw(self, screen):
         pygame.draw.polygon()
-        screen.fill
+        screen.fill.
 
     def update(self, dt):
         # sub-classes must override
