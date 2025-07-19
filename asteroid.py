5d0b514228c3c15145ae4bcf2faa9a09c16cf5ba
Spillvind <oyvinds.tommernes@gmail.com>
2025-07-19 21:45:52 +0200

    Fixed position in asteroids

--- asteroid.py
+++ asteroid.py
@@ -1,7 +1,15 @@
 from circleshape import CircleShape
 from main import pygame
 
-class Asteroid(CircleShape)
+class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
-        def draw(self, screen):
-    pygame.draw.circle()
+        
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, (255,255,255), self.position, self.radius, width = 2)
+
+
+
+    def update(self, dt):
+        self.position += self.velocity * dt
